f9686bee3fa2e5be16f0a381881b07107fcd0ece
Movsar Bekaev <bekaev.movsar@gmail.com>
2019-10-29 11:22:01 +0300

    from backups

---
+++ AbForm.designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AbForm
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.lblCopyright = New System.Windows.Forms.Label
+        Me.lblVersion = New System.Windows.Forms.Label
+        Me.lblProduct = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Label1.Location = New System.Drawing.Point(-1, 193)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Product name"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Label2.Location = New System.Drawing.Point(-1, 207)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Version"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Label3.Location = New System.Drawing.Point(-1, 234)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Copyright"
+        '
+        'lblCopyright
+        '
+        Me.lblCopyright.BackColor = System.Drawing.Color.Transparent
+        Me.lblCopyright.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblCopyright.ForeColor = System.Drawing.Color.Cornsilk
+        Me.lblCopyright.Location = New System.Drawing.Point(190, 234)
+        Me.lblCopyright.Name = "lblCopyright"
+        Me.lblCopyright.Size = New System.Drawing.Size(225, 15)
+        Me.lblCopyright.TabIndex = 7
+        Me.lblCopyright.Text = "Авторские права"
+        Me.lblCopyright.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'lblVersion
+        '
+        Me.lblVersion.BackColor = System.Drawing.Color.Transparent
+        Me.lblVersion.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblVersion.ForeColor = System.Drawing.Color.Cornsilk
+        Me.lblVersion.Location = New System.Drawing.Point(264, 207)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(151, 15)
+        Me.lblVersion.TabIndex = 6
+        Me.lblVersion.Text = "Версия"
+        Me.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'lblProduct
+        '
+        Me.lblProduct.BackColor = System.Drawing.Color.Transparent
+        Me.lblProduct.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblProduct.ForeColor = System.Drawing.Color.Cornsilk
+        Me.lblProduct.Location = New System.Drawing.Point(150, 193)
+        Me.lblProduct.Name = "lblProduct"
+        Me.lblProduct.Size = New System.Drawing.Size(265, 15)
+        Me.lblProduct.TabIndex = 5
+        Me.lblProduct.Text = "Имя продукта"
+        Me.lblProduct.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'AbForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.BackgroundImage = Global.ACAC.My.Resources.Resources.x_9ebe6ffe
+        Me.ClientSize = New System.Drawing.Size(414, 250)
+        Me.Controls.Add(Me.lblCopyright)
+        Me.Controls.Add(Me.lblVersion)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblProduct)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "AbForm"
+        Me.Padding = New System.Windows.Forms.Padding(9, 9, 9, 9)
+        Me.ShowInTaskbar = False
+        Me.Text = "AbForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblCopyright As System.Windows.Forms.Label
+    Friend WithEvents lblVersion As System.Windows.Forms.Label
+    Friend WithEvents lblProduct As System.Windows.Forms.Label
+
+End Class
---
+++ AbForm.vb
@@ -0,0 +1,37 @@
+﻿Public NotInheritable Class AbForm
+    Private Declare Function ReleaseCapture Lib "user32" () As Long
+
+    Private Declare Function SendMessage Lib "user32" Alias "SendMessageA" _
+           (ByVal hwnd As IntPtr, _
+            ByVal wMsg As Integer, _
+            ByVal wParam As Integer, _
+            ByVal lParam As Integer) As Integer
+
+    Private Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Private Const HTCAPTION As Integer = 2
+
+    Private Sub AbForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim ApplicationTitle As String
+        If My.Application.Info.Title <> "N-Code" Then
+            ApplicationTitle = My.Application.Info.Title
+        Else
+            ApplicationTitle = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+        End If
+        Me.Text = String.Format("О программе {0}", ApplicationTitle)
+        Me.lblProduct.Text = My.Application.Info.ProductName
+        Me.lblVersion.Text = String.Format("Версия {0}", My.Application.Info.Version.ToString)
+        Me.lblCopyright.Text = My.Application.Info.Copyright
+    End Sub
+
+    Private Sub AbForm_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.Close()
+    End Sub
+
+End Class
